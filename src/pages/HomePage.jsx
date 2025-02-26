3a05e58a451416e3ecc1a69ced50b38566f8abd6
Ramanand Dubey <rdx-69er@users.noreply.github.com>
2025-02-26 11:02:32 +0530

    First Commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/header';
+
+function HomePage() {
+    return (
+        <section className='container-fluid'>
+            <Header />
+        </section>
+    )
+}
+
+export default HomePage;
